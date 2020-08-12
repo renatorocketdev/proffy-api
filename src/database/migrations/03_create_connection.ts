7d5c88260f15510a049b9eecd34d7a1e82f93448
yurivolkov <yurivolkovfenrir@gmail.com>
2020-08-12 20:24:40 -0300

    fix: table name connections

--- src/database/migrations/03_create_connection.ts
+++ src/database/migrations/03_create_connection.ts
@@ -1,7 +1,7 @@
 import Knex from 'knex';
 
 export async function up(knex: Knex){
-  return knex.schema.createTable('connection', table => {
+  return knex.schema.createTable('connections', table => {
     table.increments('id').primary();
 
     table.integer('user_id')
@@ -12,7 +12,7 @@ export async function up(knex: Knex){
       .onDelete('CASCADE');
 
     table.timestamp('created_at')
-      .defaultTo('now()')
+      .defaultTo(knex.raw('CURRENT_TIMESTAMP'))
       .notNullable();
   });
 }
